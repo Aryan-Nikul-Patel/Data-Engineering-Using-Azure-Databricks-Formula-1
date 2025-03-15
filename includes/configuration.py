28dd9045585278e3a29ac76435fc336964047f6c
Aryan-Nikul-Patel <Aryan-Nikul-Patel>
2025-03-15 08:20:34 +0000

    includes

---
+++ includes/configuration.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/adcd4hpstorage/raw"
+processed_folder_path = "/mnt/adcd4hpstorage/processed"
+presentation_folder_path = "/mnt/adcd4hpstorage/presentation"
